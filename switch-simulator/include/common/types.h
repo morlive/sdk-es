072a86af24bf421feabeb80b8ca1acb2e634f8b3
morris <morris.elkanaev@gmail.com>
2025-03-10 23:52:14 +0200

    modified

--- switch-simulator/include/common/types.h
+++ switch-simulator/include/common/types.h
@@ -21,29 +21,29 @@ typedef enum
   STATUS_TIMEOUT,                 /**< Operation timed out */
   STATUS_TABLE_FULL,              /**< Table is full */
   STATUS_ALREADY_EXISTS,          /**< Entry already exists */
-  STATUS_NOT_FOUND,               /**< Entry not found */
+  STATUS_NOT_FOUND                /**< Entry not found */
 } status_t;
 
 typedef struct 
 {
-    uint8_t addr[6];
+  uint8_t addr[6];
 } mac_addr_t;
 
 typedef uint32_t ipv4_addr_t;
 
 typedef struct 
 {
-    uint8_t addr[16];
+  uint8_t addr[16];
 } ipv6_addr_t;
 
 typedef uint16_t port_id_t;
 typedef uint16_t vlan_id_t;
 typedef uint32_t switch_id_t;
 
-#define MAX_PORTS 64
-#define MAX_VLANS 4096
+#define MAX_PORTS             64
+#define MAX_VLANS             4096
 #define MAX_MAC_TABLE_ENTRIES 8192
-#define MAX_PACKET_SIZE 9216  // Support for jumbo frames
+#define MAX_PACKET_SIZE       9216  // Support for jumbo frames
 
 
 
