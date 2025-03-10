05c4cf91be5112643afe3230aabdd203f774aa5a
morris <morris.elkanaev@gmail.com>
2025-03-10 15:09:08 +0200

    modified

---
+++ switch-simulator/include/common/types.h
@@ -0,0 +1,13 @@
+/**
+ * @file types.h
+ * @brief Base type definitions for switch simulator
+ */
+
+#ifndef SWITCH_SIM_TYPES_H
+#define SWITCH_SIM_TYPES_H
+
+
+
+
+
+#endif   /* SWITCH_SIM_TYPES_H */
