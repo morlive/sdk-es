36248797cadd36c5ead5eaf64775980b5e64114f
morris <morris.elkanaev@gmail.com>
2025-03-10 23:47:35 +0200

    modified adding basic types

--- switch-simulator/include/common/types.h
+++ switch-simulator/include/common/types.h
@@ -6,7 +6,49 @@
 #ifndef SWITCH_SIM_TYPES_H
 #define SWITCH_SIM_TYPES_H
 
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
 
+
+/**
+ * @brief Status codes for API function returns
+ */
+typedef enum
+{
+  STATUS_SUCCESS = 0,             /**< Operation completed successfully */
+  STATUS_FAILURE,                 /**< Generic failure */
+  STATUS_INVALID_PARAMETER,       /**< Invalid parameter provided */
+  STATUS_INSUFFICIENT_RESOURCES,  /**< Not enough resources to complete operation */
+  STATUS_NOT_INITIALIZED,         /**< Component not initialized */
+  STATUS_NOT_IMPLEMENTED,         /**< Feature not implemented */
+  STATUS_TIMEOUT,                 /**< Operation timed out */
+  STATUS_TABLE_FULL,              /**< Table is full */
+  STATUS_ALREADY_EXISTS,          /**< Entry already exists */
+  STATUS_NOT_FOUND,               /**< Entry not found */
+} status_t;
+
+
+typedef struct 
+{
+    uint8_t addr[6];
+} mac_addr_t;
+
+typedef uint32_t ipv4_addr_t;
+
+typedef struct 
+{
+    uint8_t addr[16];
+} ipv6_addr_t;
+
+typedef uint16_t port_id_t;
+typedef uint16_t vlan_id_t;
+typedef uint32_t switch_id_t;
+
+#define MAX_PORTS 64
+#define MAX_VLANS 4096
+#define MAX_MAC_TABLE_ENTRIES 8192
+#define MAX_PACKET_SIZE 9216  // Support for jumbo frames
 
 
 
